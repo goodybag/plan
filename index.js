db6975f240678a00421907d780f25b4b29206d21
John Fawcett <jrf0110@gmail.com>
2014-12-04 11:58:53 -0600

    amdified

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-module.exports = require('./lib/plan');
-module.exports.Reduce = require('./lib/plan-reduce');
+/**
+ * Plan.js
+ */
+
+if ( typeof module === "object" && module && typeof module.exports === "object" ){
+  var isNode = true, define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+define( function( require, exports, module ){
+  module.exports = require('./lib/plan');
+  module.exports.Reduce = require('./lib/plan-reduce');
+  return module.exports;
+});
